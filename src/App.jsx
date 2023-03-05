c313b1a3c8bfa6d622ba233f9e4288cd7cdd6397
EBliujus <ev.bliujus@gmail.com>
2023-03-05 15:15:57 +0200

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.scss";
-import NewAcc from "./Components/Create";
 
-
-
-
-const KEY = 'costumer';
 
 function App() {
 
-  const [list, setList] = useState(null);
-  const [lastRefresh, setLastRefresh] = useState(Date.now());
-  const [newAccData, setNewAccData] = useState(null);
-  const [deleteData, setDeleteData] = useState(null);
-  const [modalData, setModalData] = useState(null);
-  const [editData, setEditData] = useState(null);
-
-  useEffect(() => {
-    setList(read(KEY));
-  }, [lastRefresh]);
-
-  useEffect (() => {
-    if (null === newAccData) {
-      return;
-    }
-    NewAcc(KEY, newAccData);
-    setLastRefresh(Date.now())
-  }, [newAccData]);
-
-  useEffect(() => {
-    if (null === deleteData) {
-      return;
-    }
-    // tipo klientas(narsykle)
-    setList(l => l.filter(d => deleteData.id !== d.id))
-    // tipo serveris
-    destroy(KEY, deleteData.id);
-    setLastResfresh(Date.now())
-  }, [deleteData]);
-
-  useEffect(() => {
-    if (null === editData) {
-      return;
-    }
-    edit(KEY, editData, editData.id);
-    setLastResfresh(Date.now())
-  }, [editData]);
+    const [vardas, setVardas] = useState('');
+    const [pavarde, setPavarde] = useState('');
+    const [suma, setSuma] = useState(0);
+    const [klientas, setKlientas] = useState([]);
 
 
 
   return (
     <>
-      <div className="container">
-      <div className="row">
-          <div className="col-4">
-              <NewAcc setNewAccData={setNewAccData} />
+          <div className="card mt-5">
+            <div className="card-header">
+                <h3>Nauja Sąskaita</h3>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+              <input type="text" className="form-control" id="floatingInput" placeholder="Vardas"/>
+              </li>
+              <li className="list-group-item">
+                  <input type="text" className="form-control" id="floatingInput" placeholder="Pavardė"/>
+              </li>
+              <button type="button" className="btn btn-primary btn-sm">Atidaryti Sąskaita</button>
+            </ul>
           </div>
-      <div className="col-8">
-          <List list={list} setDeleteData={setDeleteData} setModalData ={setModalData}/>
-      </div>
-      </div>
-  </div>
-  <Edit modalData={modalData} setModalData={setModalData} setEditData={setEditData}/>
     </>
   );
 }
