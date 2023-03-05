5aaac8064b388da9e9041dcc91d4ab278b249631
EBliujus <ev.bliujus@gmail.com>
2023-03-05 14:32:55 +0200

    Bandymas I

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,69 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
-import newAcc from "./Components/NewAcc";
+import NewAcc from "./Components/Create";
 
-const KEY = 'klientas';
+
+
+
+const KEY = 'costumer';
 
 function App() {
+
+  const [list, setList] = useState(null);
+  const [lastRefresh, setLastRefresh] = useState(Date.now());
+  const [newAccData, setNewAccData] = useState(null);
+  const [deleteData, setDeleteData] = useState(null);
+  const [modalData, setModalData] = useState(null);
+  const [editData, setEditData] = useState(null);
+
   useEffect(() => {
     setList(read(KEY));
-  },[lastRefresh]);
+  }, [lastRefresh]);
 
-  useEffect(() => {
-    if (null === newAcc) {
+  useEffect (() => {
+    if (null === newAccData) {
       return;
     }
-    
-  })
-  
-  return (
+    NewAcc(KEY, newAccData);
+    setLastRefresh(Date.now())
+  }, [newAccData]);
 
+  useEffect(() => {
+    if (null === deleteData) {
+      return;
+    }
+    // tipo klientas(narsykle)
+    setList(l => l.filter(d => deleteData.id !== d.id))
+    // tipo serveris
+    destroy(KEY, deleteData.id);
+    setLastResfresh(Date.now())
+  }, [deleteData]);
+
+  useEffect(() => {
+    if (null === editData) {
+      return;
+    }
+    edit(KEY, editData, editData.id);
+    setLastResfresh(Date.now())
+  }, [editData]);
+
+
+
+  return (
+    <>
+      <div className="container">
+      <div className="row">
+          <div className="col-4">
+              <NewAcc setNewAccData={setNewAccData} />
+          </div>
+      <div className="col-8">
+          <List list={list} setDeleteData={setDeleteData} setModalData ={setModalData}/>
+      </div>
+      </div>
+  </div>
+  <Edit modalData={modalData} setModalData={setModalData} setEditData={setEditData}/>
+    </>
   );
 }
 
